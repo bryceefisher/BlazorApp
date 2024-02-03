2a0b2235813494dd37e9082e305f32ad341fdb7f
Bryce Fisher <101945315+bryceefisher@users.noreply.github.com>
2024-02-03 15:48:43 -0800

    Update MysqlhomebrewContext.cs

--- BlazorApp/Data/MysqlhomebrewContext.cs
+++ BlazorApp/Data/MysqlhomebrewContext.cs
@@ -31,7 +31,7 @@ public partial class MysqlhomebrewContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=homebrewrecipe.mysql.database.azure.com;userid=bryce;password=Lakers11055!;database=mysqlhomebrew", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+        => optionsBuilder.UseMySql({connection string}, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
